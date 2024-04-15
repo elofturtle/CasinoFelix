38b2f01cbbb80e492c0597de7c38304a72bc4ac3
elofturtle <47484310+elofturtle@users.noreply.github.com>
2024-04-15 12:11:03 +0200

    Update PlayerService.java

--- src/main/java/fk/examples/onlinecasino/service/PlayerService.java
+++ src/main/java/fk/examples/onlinecasino/service/PlayerService.java
@@ -1,5 +1,21 @@
 package fk.examples.onlinecasino.service;
 
-public class PlayerService {
+import fk.examples.onlinecasino.model.Player;
 
+public class PlayerService {
+	public boolean  transfer(Player p1, Player p2, int amount) {
+		if (amount < 0) {
+			throw new IllegalArgumentException("Du får inte överföra skulder");
+		}
+		if(p1 == p2) {
+			throw new IllegalArgumentException("Systemet tillåter inte transaktioner till dig själv");
+		}
+		int p1Wallet = p1.getCredit();
+		if (p1Wallet >= amount) {
+			p1.setCredit(p1Wallet - amount);
+			p2.setCredit(p2.getCredit() +  p1Wallet);
+			return true;
+		}
+		else return false;
+	}
 }
