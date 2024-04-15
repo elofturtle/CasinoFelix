152926770852df5a1d358acae5ccaff0508409dd
elofturtle <47484310+elofturtle@users.noreply.github.com>
2024-04-15 12:10:28 +0200

    Update PlayerServiceTest.java

--- src/test/java/casino/PlayerServiceTest.java
+++ src/test/java/casino/PlayerServiceTest.java
@@ -1,8 +1,10 @@
 package casino;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
+
 
 import fk.examples.onlinecasino.model.Player;
 import fk.examples.onlinecasino.service.PlayerService;
@@ -20,8 +22,48 @@ public class PlayerServiceTest {
 		Player p1 = new Player("0001", "Robin", "Hood", 100, "robin.hood@gmail.com", "monkey123");
 		Player p2 = new Player("0002", "Joakim", "von Anka", 1_000_000, "rikjoakim@gmail.com", "anka123");
 
-		ps.transfer(p1, p2, 100);
-		assertTrue(false);
+		boolean result = ps.transfer(p1, p2, 100);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testPlayerSaldoOverdrawn() {
+		Player p1 = new Player("0001", "Robin", "Hood", 100, "robin.hood@gmail.com", "monkey123");
+		Player p2 = new Player("0002", "Joakim", "von Anka", 1_000_000, "rikjoakim@gmail.com", "anka123");
+
+		boolean result = ps.transfer(p1, p2, 100000);
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testPlayerSaldoIntegrity() {
+		Player p1 = new Player("0001", "Robin", "Hood", 100, "robin.hood@gmail.com", "monkey123");
+		Player p2 = new Player("0002", "Joakim", "von Anka", 1_000_000, "rikjoakim@gmail.com", "anka123");
+		
+		int p1Wallet = p1.getCredit();
+		int p2Wallet = p2.getCredit();
+		int expectedTotal = p1Wallet + p2Wallet;
+		
+		boolean result = ps.transfer(p1, p2, 100);
+		
+		int newTotal = p1.getCredit() + p2.getCredit();
+		
+		assertTrue(expectedTotal == newTotal);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testPlayerNoNegativeTransactions() {
+		Player p1 = new Player("0001", "Robin", "Hood", 100, "robin.hood@gmail.com", "monkey123");
+		Player p2 = new Player("0002", "Joakim", "von Anka", 1_000_000, "rikjoakim@gmail.com", "anka123");
+		
+		ps.transfer(p1, p2, -100);
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testPlayerDifferentPlayers() {
+		Player p1 = new Player("0001", "Robin", "Hood", 100, "robin.hood@gmail.com", "monkey123");
+		Player p2 = new Player("0002", "Joakim", "von Anka", 1_000_000, "rikjoakim@gmail.com", "anka123");
+
+		ps.transfer(p1, p1, 100);
+	}
 }
